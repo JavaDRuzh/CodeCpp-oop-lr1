7564edb5a1400aa1e82758457da220175721b0b6
Vladimir Druzhinin <dialog.txt@gmail.com>
2017-04-27 23:15:20 +0700

    let's start

---
+++ cmake-build-debug/CMakeFiles/CodeCpp_oop_lr1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CodeCpp_oop_lr1.dir/main.cpp.o"
+  "CodeCpp_oop_lr1.pdb"
+  "CodeCpp_oop_lr1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CodeCpp_oop_lr1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
